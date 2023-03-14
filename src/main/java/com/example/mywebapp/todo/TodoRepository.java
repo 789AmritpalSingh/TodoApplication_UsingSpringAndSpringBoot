609bd8fe142edc778af34a74a57f4851c74a266d
Amritpal Singh <asvarpal450@gmail.com>
2023-03-13 23:26:40 -0400

    todo application updated

--- src/main/java/com/example/mywebapp/todo/TodoRepository.java
+++ src/main/java/com/example/mywebapp/todo/TodoRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo,Integer> {
-    public List<Todo> findByUserName(String userName);
+    List<Todo> findByUserName(String userName);
 
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 insert into todo(ID,USER_NAME,DESCRIPTION,TARGET_DATE,DONE) values (10001,'Divya Sharma','Learn AWS',CURRENT_DATE(),false);
 
 insert into todo(ID,USER_NAME,DESCRIPTION,TARGET_DATE,DONE) values (10002,'Amritpal Singh','Learn Java',CURRENT_DATE(),false);
+
+insert into todo(ID,USER_NAME,DESCRIPTION,TARGET_DATE,DONE) values (10003,'Balwinder Kaur','Learn Spring',CURRENT_DATE(),false);
+
